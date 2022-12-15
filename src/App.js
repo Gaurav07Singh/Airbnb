fa517407f92bd4d98b9ca254a88e5d0c08e35354
Gaurav Singh <gks858687@gmail.com>
2022-12-15 14:38:20 +0530

    Added card component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
-
+import Card from './Components/Card';
 
 function App() {
   return (
     <div>
          <Navbar/>
          <Hero/>
+
+         <div className = "cards--div">
+
+        
+         <Card
+         img = "./scuba-diving.png"
+         rating = "5.0"
+         noOfPeople = "16"
+         country = "USA"
+         topic = "Life lessons with Ankur Warikoo"
+         price= "2500"
+         />
+
+         <Card
+         rating = "4"
+         noOfPeople = "14"
+         country = "India"
+         topic = "Swim With us"
+         price = "8500"
+         />
+
+        <Card
+         rating = "3.9"
+         noOfPeople = "140"
+         country = "Nigeria"
+         topic = "Devotion"
+         price = "485"
+         />
+
+         </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import star from "../images/star.png";
+import forest from "../images/japan-forest.png";
+import scuba from "../images/scuba-diving.png";
+import canyon from "../images/grand-canyon.png";
+import bike from "../images/mountain-bike.png";
+
+
+function Card(props)
+{
+    return(
+        <div class = "main-2">
+            <div className="card">
+                <img src = {`../images/${props.img}`}  className="card--image" />
+                <div className="card--stats">
+                    <img src={star} class = "card--star"/>
+                    <span>{props.rating}</span>
+                    <span> ({props.noOfPeople}) </span>
+                    <span>{props.country}</span>
+                </div>
+                <p>{props.topic}</p>
+                <p><span className="bold">From $<span>{props.price}</span></span> / person</p>
+            </div>
+
+            {/* <div className="card">
+                <img src={ankur} className="card--image" />
+                <div className="card--stats">
+                    <img src={star} class = "card--star"/>
+                    <span>{props.rating}</span>
+                    <span> ({props.noOfPeople}) </span>
+                    <span>{props.country}</span>
+                </div>
+                <p>{props.topic}</p>
+                <p><span className="bold">From $<span>{props.price}</span></span> / person</p>
+            </div> */}
+
+
+        </div>
+
+    )
+}
+
+export default Card;
